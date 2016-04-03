2c20603b8ed88f1d86e6b51d378b27a8694e49b2
acunningham065 <acunningham065@gmail.com>
2016-04-03 20:02:07 +0100

    Add files via Upload

---
+++ Battleships/Battleships/frm_Battleships.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Battleships
+{
+    partial class frm_Battleships
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Battleships));
+            this.flp_Board = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_PlayGame = new System.Windows.Forms.Button();
+            this.imgLst_Ships = new System.Windows.Forms.ImageList(this.components);
+            this.lbl_Score = new System.Windows.Forms.Label();
+            this.lbl_Missiles = new System.Windows.Forms.Label();
+            this.btn_EasyLevel = new System.Windows.Forms.Button();
+            this.btn_IntermediateLevel = new System.Windows.Forms.Button();
+            this.btn_HardLevel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flp_Board
+            // 
+            this.flp_Board.Location = new System.Drawing.Point(322, 41);
+            this.flp_Board.Name = "flp_Board";
+            this.flp_Board.Size = new System.Drawing.Size(600, 560);
+            this.flp_Board.TabIndex = 0;
+            // 
+            // btn_PlayGame
+            // 
+            this.btn_PlayGame.Enabled = false;
+            this.btn_PlayGame.Location = new System.Drawing.Point(22, 41);
+            this.btn_PlayGame.Name = "btn_PlayGame";
+            this.btn_PlayGame.Size = new System.Drawing.Size(185, 48);
+            this.btn_PlayGame.TabIndex = 0;
+            this.btn_PlayGame.Text = "Play";
+            this.btn_PlayGame.UseVisualStyleBackColor = true;
+            this.btn_PlayGame.Click += new System.EventHandler(this.btn_PlayGame_Click);
+            // 
+            // imgLst_Ships
+            // 
+            this.imgLst_Ships.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgLst_Ships.ImageStream")));
+            this.imgLst_Ships.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgLst_Ships.Images.SetKeyName(0, "miss.jpg");
+            this.imgLst_Ships.Images.SetKeyName(1, "submarine.jpg");
+            this.imgLst_Ships.Images.SetKeyName(2, "frigate.png");
+            this.imgLst_Ships.Images.SetKeyName(3, "destroyer.jpg");
+            this.imgLst_Ships.Images.SetKeyName(4, "battleship.jpg");
+            // 
+            // lbl_Score
+            // 
+            this.lbl_Score.AutoSize = true;
+            this.lbl_Score.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Score.Location = new System.Drawing.Point(17, 530);
+            this.lbl_Score.Name = "lbl_Score";
+            this.lbl_Score.Size = new System.Drawing.Size(78, 26);
+            this.lbl_Score.TabIndex = 1;
+            this.lbl_Score.Text = "Score:";
+            // 
+            // lbl_Missiles
+            // 
+            this.lbl_Missiles.AutoSize = true;
+            this.lbl_Missiles.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Missiles.Location = new System.Drawing.Point(17, 570);
+            this.lbl_Missiles.Name = "lbl_Missiles";
+            this.lbl_Missiles.Size = new System.Drawing.Size(102, 26);
+            this.lbl_Missiles.TabIndex = 2;
+            this.lbl_Missiles.Text = "Missiles:";
+            // 
+            // btn_EasyLevel
+            // 
+            this.btn_EasyLevel.Location = new System.Drawing.Point(22, 125);
+            this.btn_EasyLevel.Name = "btn_EasyLevel";
+            this.btn_EasyLevel.Size = new System.Drawing.Size(185, 48);
+            this.btn_EasyLevel.TabIndex = 3;
+            this.btn_EasyLevel.Text = "Easy Level";
+            this.btn_EasyLevel.UseVisualStyleBackColor = true;
+            this.btn_EasyLevel.Click += new System.EventHandler(this.btn_EasyLevel_Click);
+            // 
+            // btn_IntermediateLevel
+            // 
+            this.btn_IntermediateLevel.Location = new System.Drawing.Point(22, 209);
+            this.btn_IntermediateLevel.Name = "btn_IntermediateLevel";
+            this.btn_IntermediateLevel.Size = new System.Drawing.Size(185, 48);
+            this.btn_IntermediateLevel.TabIndex = 4;
+            this.btn_IntermediateLevel.Text = "Intermediate Level";
+            this.btn_IntermediateLevel.UseVisualStyleBackColor = true;
+            this.btn_IntermediateLevel.Click += new System.EventHandler(this.btn_IntermediateLevel_Click);
+            // 
+            // btn_HardLevel
+            // 
+            this.btn_HardLevel.Location = new System.Drawing.Point(22, 293);
+            this.btn_HardLevel.Name = "btn_HardLevel";
+            this.btn_HardLevel.Size = new System.Drawing.Size(185, 48);
+            this.btn_HardLevel.TabIndex = 5;
+            this.btn_HardLevel.Text = "Hard Level";
+            this.btn_HardLevel.UseVisualStyleBackColor = true;
+            this.btn_HardLevel.Click += new System.EventHandler(this.btn_HardLevel_Click);
+            // 
+            // frm_Battleships
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 687);
+            this.Controls.Add(this.btn_HardLevel);
+            this.Controls.Add(this.btn_IntermediateLevel);
+            this.Controls.Add(this.btn_EasyLevel);
+            this.Controls.Add(this.lbl_Missiles);
+            this.Controls.Add(this.lbl_Score);
+            this.Controls.Add(this.btn_PlayGame);
+            this.Controls.Add(this.flp_Board);
+            this.Name = "frm_Battleships";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flp_Board;
+        private System.Windows.Forms.Button btn_PlayGame;
+        private System.Windows.Forms.ImageList imgLst_Ships;
+        private System.Windows.Forms.Label lbl_Score;
+        private System.Windows.Forms.Label lbl_Missiles;
+        private System.Windows.Forms.Button btn_EasyLevel;
+        private System.Windows.Forms.Button btn_IntermediateLevel;
+        private System.Windows.Forms.Button btn_HardLevel;
+    }
+}
+
---
+++ Battleships/Battleships/frm_Battleships.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Battleships
+{
+    public partial class frm_Battleships : Form
+    {
+        static int seaSize = 100;
+        int score = 0;
+        int remainingMissiles = 70;
+        int usedMissiles = 0;
+
+        int[] boardSquares = new int[seaSize];
+
+
+        public frm_Battleships()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btn_PlayGame_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int shipType = 0;
+
+            //Disable Play Button
+            btn_PlayGame.Enabled = false;
+            btn_EasyLevel.Enabled = false;
+            btn_IntermediateLevel.Enabled = false;
+            btn_HardLevel.Enabled = false;
+
+            //For all of the squares
+            for (int currentSquare = 0; currentSquare < seaSize; currentSquare++)
+            {
+                //Create and add a box to screen
+                CreateBox(currentSquare, shipType);
+
+                //Decide the ship type if a ship exists
+                shipType = rnd.Next(1, 16);
+
+
+                switch (shipType)
+                {
+                   
+                    case 1:
+                        //2 length boat
+                        if (currentSquare + 1 < seaSize)
+                        {
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+                        }
+                        else
+                        {
+                            shipType = 0;
+
+                            CreateBox(currentSquare, shipType);
+                        }
+                        
+                        break;
+
+
+                    case 2:
+                        //3 length boat
+                        if (currentSquare + 2 < seaSize)
+                        {
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                        }
+                        else
+                        {
+                            shipType = 0;
+
+                            CreateBox(currentSquare, shipType);
+                        }
+                        
+                        break;
+
+
+                    case 3:
+                        //4 length boat
+                        if (currentSquare + 3 < seaSize)
+                        {
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+                        }
+                        else
+                        {
+                            shipType = 0;
+
+                            CreateBox(currentSquare, shipType);
+                        }
+                                                
+                        break;
+
+
+                    case 4:
+                        //5 length boat
+                        if (currentSquare + 4 < seaSize)
+                        {
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                            currentSquare++;
+
+                            CreateBox(currentSquare, shipType);
+
+                        }
+                        else
+                        {
+                            shipType = 0;
+
+                            CreateBox(currentSquare, shipType);
+                        }
+                        
+                        break;
+
+                    default:
+                        //No boat
+                        break;
+
+                }//End Switch
+
+            }//End For
+
+        }//End PlayGameClick
+
+
+
+        private void CreateBox(int currentSquare, int shipType)
+        {
+            int boxSize = 50;
+
+            //Create the box
+            PictureBox pbx = new PictureBox();
+
+            pbx.Height = boxSize;
+            pbx.Width = boxSize;
+
+            //Assign the name
+            pbx.Name = currentSquare.ToString();
+
+            //Assign to the array
+            boardSquares[currentSquare] = shipType;
+
+            //Add colour
+            pbx.BackColor = Color.Black;
+
+            //Add to board
+            flp_Board.Controls.Add(pbx);
+
+            //Add mouse click handler
+            pbx.MouseClick += pbx_MouseClick;
+        }
+
+
+
+        void pbx_MouseClick(object sender, MouseEventArgs e)
+        {
+            PictureBox pbxClicked = sender as PictureBox;
+
+            //Ensure picture is positioned and sized correctly
+            pbxClicked.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            if (pbxClicked.Image == null)
+            {
+
+                //Check if missiles remain
+                if (usedMissiles < remainingMissiles)
+                {
+                    //Find the array position
+                    int arrayPositionOfBox = Convert.ToInt32(pbxClicked.Name.ToString());
+
+
+                    switch (boardSquares[arrayPositionOfBox])
+                    {
+
+                        case 1://Hit a submarine
+
+                            pbxClicked.Image = imgLst_Ships.Images[1];
+
+                            //Add Score
+                            score = score + 10;
+
+                            //Increase number of missiles used
+                            usedMissiles++;
+
+                            break;
+
+                        case 2://Hit a frigate
+
+                            pbxClicked.Image = imgLst_Ships.Images[2];
+
+                            //Add Score
+                            score = score + 20;
+
+                            //Increase number of missiles used
+                            usedMissiles++;
+
+                            break;
+
+                        case 3://Hit a destroyer
+
+                            pbxClicked.Image = imgLst_Ships.Images[3];
+
+                            //Add Score
+                            score = score + 30;
+
+                            //Increase number of missiles used
+                            usedMissiles++;
+
+                            break;
+
+                        case 4://Hit a Battleship
+
+                            pbxClicked.Image = imgLst_Ships.Images[4];
+
+                            //Add Score
+                            score = score + 40;
+
+                            //Increase number of missiles used
+                            usedMissiles++;
+
+                            break;
+
+                        default:
+                            //Miss
+                            pbxClicked.Image = imgLst_Ships.Images[0];
+
+                            //Increase number of missiles used
+                            usedMissiles++;
+
+                            break;
+
+                    }//End Switch
+
+                    //Set labels
+                    lbl_Score.Text = "Score: " + score.ToString();
+                    lbl_Missiles.Text = "Missiles: " + usedMissiles.ToString() + "/" + remainingMissiles.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Game Over");
+
+                    //Re-enable buttons
+                    btn_PlayGame.Enabled = true;
+                    btn_EasyLevel.Enabled = true;
+                    btn_IntermediateLevel.Enabled = true;
+                    btn_HardLevel.Enabled = true;
+
+                    //Reset variables
+                    flp_Board.Controls.Clear();
+                    score = 0;
+                    usedMissiles = 0;
+
+                    lbl_Score.Text = "Score: " + score.ToString();
+                    lbl_Missiles.Text = "Missiles: " + usedMissiles.ToString() + "/" + remainingMissiles.ToString();
+
+                }//End if
+
+            }
+            else
+            {
+                MessageBox.Show("That was already clicked");
+
+                usedMissiles++;
+            }//End if
+
+        }//End pbx_MouseClick
+
+
+
+        private void btn_EasyLevel_Click(object sender, EventArgs e)
+        {
+            remainingMissiles = 70;
+
+            btn_PlayGame.Enabled = true;
+        }
+
+
+
+        private void btn_IntermediateLevel_Click(object sender, EventArgs e)
+        {
+            remainingMissiles = 50;
+
+            btn_PlayGame.Enabled = true;
+
+        }
+
+
+
+        private void btn_HardLevel_Click(object sender, EventArgs e)
+        {
+            remainingMissiles = 30;
+
+            btn_PlayGame.Enabled = true;
+        }
+
+
+
+    }
+}
+
